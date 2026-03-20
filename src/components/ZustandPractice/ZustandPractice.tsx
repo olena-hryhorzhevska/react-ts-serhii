f94297797cb3599fea0bda07d5a2cbdf1d61cdb7
Olena Hryhorzhevska <elenahry8@gmail.com>
2026-03-20 15:59:50 -0400

    zustand persist

--- src/components/ZustandPractice/ZustandPractice.tsx
+++ src/components/ZustandPractice/ZustandPractice.tsx
@@ -1,20 +1,53 @@
-import { useCounterStore } from '../store/useCounterStore';
-import { useShallow } from 'zustand/shallow';
+// persist - localStorage/sessionStorage
+
+import { useTodoStore } from '../store/useCounterStore';
 
 export default function ZustandPractice() {
-  // const country = useCounterStore(state => state.country); примитив
-
-  const data = useCounterStore(
-    useShallow(state => ({
-      country: state.country,
-      name: state.user.name,
-    }))
-  );
+  const inputValue = useTodoStore(state => state.inputValue);
+  const todos = useTodoStore(state => state.todos);
+  const setInputValue = useTodoStore(state => state.setInputValue);
+  const addTodo = useTodoStore(state => state.addTodo);
+  const removeTodo = useTodoStore(state => state.removeTodo);
 
   return (
     <div>
-      <h1>Country: {data.country}</h1>
-      <h2>Name: {data.name}</h2>
+      <h1>Todo without persist</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
+        placeholder="Enter todo"
+      />
+      <button onClick={addTodo}>Add todo</button>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            {todo.title}
+            <button onClick={() => removeTodo(todo.id)}>Delete todo</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+// import { useCounterStore } from '../store/useCounterStore';
+// import { useShallow } from 'zustand/shallow';
+
+// export default function ZustandPractice() {
+//   // const country = useCounterStore(state => state.country); примитив
+
+//   const data = useCounterStore(
+//     useShallow(state => ({
+//       country: state.country,
+//       name: state.user.name,
+//     }))
+//   );
+
+//   return (
+//     <div>
+//       <h1>Country: {data.country}</h1>
+//       <h2>Name: {data.name}</h2>
+//     </div>
+//   );
+// }
--- src/components/store/useCounterStore.ts
+++ src/components/store/useCounterStore.ts
@@ -1,33 +1,82 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-type CounterStore = {
-  country: string;
-  user: {
-    name: string;
-    age: number;
-  };
-  updateCountry: (country: string) => void;
-  updatedName: (name: string) => void;
+type Todo = {
+  id: number;
+  title: string;
 };
 
-export const useCounterStore = create<CounterStore>(set => ({
-  country: 'USA',
-  updateCountry: country => set({ country: country }),
-  user: {
-    name: 'John',
-    age: 30,
-  },
-  updatedName: name =>
-    set(state => ({
-      user: {
-        ...state.user,
-        name: name,
-      },
-    })),
-  
-  // updatedName: name => set(() => ({
-  //   user: {
-  //     name,
-  //   }
-  // })) НЕЛЬЗЯ ТАК ДЕЛАТЬ, ПОТОМУ ЧТО ТАК МЫ ПЕРЕЗАПИСЫВАЕМ ВЕСЬ ОБЪЕКТ user И У НАС ПРОПАДАЕТ age
-}));
+type TodoStore = {
+  todos: Todo[];
+  inputValue: string;
+  setInputValue: (value: string) => void;
+  addTodo: () => void;
+  removeTodo: (id: number) => void;
+};
+
+export const useTodoStore = create<TodoStore>()(
+  persist(
+    set => ({
+      todos: [],
+      inputValue: '',
+
+      setInputValue: value => set({ inputValue: value }),
+
+      addTodo: () =>
+        set(state => {
+          if (!state.inputValue.trim()) return state; // Проверяем, что input не пустой
+          return {
+            todos: [
+              ...state.todos,
+              {
+                id: Date.now(),
+                title: state.inputValue,
+              },
+            ],
+            inputValue: '', // Очищаем input после добавления
+          };
+        }),
+      removeTodo: id =>
+        set(state => ({
+          todos: state.todos.filter(todo => todo.id !== id),
+        })),
+    }),
+    {
+      name: 'todo-storage', // имя для localStorage
+    }
+  )
+);
+
+// import { create } from 'zustand';
+
+// type CounterStore = {
+//   country: string;
+//   user: {
+//     name: string;
+//     age: number;
+//   };
+//   updateCountry: (country: string) => void;
+//   updatedName: (name: string) => void;
+// };
+
+// export const useCounterStore = create<CounterStore>(set => ({
+//   country: 'USA',
+//   updateCountry: country => set({ country: country }),
+//   user: {
+//     name: 'John',
+//     age: 30,
+//   },
+//   updatedName: name =>
+//     set(state => ({
+//       user: {
+//         ...state.user,
+//         name: name,
+//       },
+//     })),
+
+// updatedName: name => set(() => ({
+//   user: {
+//     name,
+//   }
+// })) НЕЛЬЗЯ ТАК ДЕЛАТЬ, ПОТОМУ ЧТО ТАК МЫ ПЕРЕЗАПИСЫВАЕМ ВЕСЬ ОБЪЕКТ user И У НАС ПРОПАДАЕТ age
+// }));
